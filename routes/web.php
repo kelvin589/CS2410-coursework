f7dfe567595032420f5a0fa6acc89ce5816e9b9f
kelvin589 <190081735@aston.ac.uk>
2021-04-06 17:15:03 +0100

    Route UserController functions

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('list', 'App\Http\Controllers\UserController@list');
+Route::get('show/{id}', 'App\Http\Controllers\UserController@show');
