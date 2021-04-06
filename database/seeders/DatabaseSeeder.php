b0f1ab21217ad7c0e1063f92a271e454514037b1
kelvin589 <190081735@aston.ac.uk>
2021-04-06 21:19:01 +0100

    Seeder for animals table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use CreateAnimalsTable;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UsersTableSeeder::class);
+        $this->call(AnimalsTableSeeder::class);
     }
 }
