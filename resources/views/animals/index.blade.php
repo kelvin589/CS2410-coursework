528998ec4e264ab0a2a5dfa2ecf4326cb84bdb44
kelvin589 <190081735@aston.ac.uk>
2021-04-13 22:47:53 +0100

    Update show all animals view

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -1,65 +1,64 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">Display all animals</div>
-                <div class="card-body">
-                    @if (session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    @if (session('danger'))
-                        <div class="alert alert-danger">
-                            {{ session('danger') }}
-                        </div>
-                    @endif
-
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Date of Birth</th>
-                                <th>Availability</th>
-                                <th>Adopted By</th>
-                                <th colspan="3">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($animals as $animal)
-                                <tr>
-                                    <td>{{ $animal['name']}}</td>
-                                    <td>{{ $animal['date_of_birth']}}</td>
-                                    @if($animal['available'])
-                                        <td>Available</td>
-                                    @else
-                                        <td>Unavailable</td>
-                                    @endif
-                                    @if($animal['user_name'])
-                                        <td>{{ $animal['user_name']}}</td>
-                                    @else
-                                        <td>Not Adopted</td>
-                                    @endif
-                                    <td>
-                                        <a href="{{ route('animals.show', ['animal' => $animal['id']]) }}" class="btn btn-primary">Details</a>
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('animals.edit', ['animal' => $animal['id']]) }}" class="btn btn-warning">Edit</a>
-                                    </td>
-                                    <td>
-                                        <form action="{{ action([App\Http\Controllers\AnimalController::class, 'destroy'], ['animal' => $animal['id']]) }}" method="post">
-                                            @csrf
-                                            <input name="_method" type="hidden" value="DELETE"> <button class="btn btn-danger" type="submit">Delete</button>
-                                        </form> 
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+            <h1 class="centre">All Animals</h1>
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
                 </div>
-            </div>
+            @endif
+            @if (session('danger'))
+                <div class="alert alert-danger">
+                    {{ session('danger') }}
+                </div>
+            @endif
+
+            <table class="table table-striped table-bordered table-hover table-pink">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Date of Birth</th>
+                        <th>Availability</th>
+                        <th>Adopted By</th>
+                        <th colspan="3">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($animals as $animal)
+                        <tr>
+                            <td>{{ $animal['id'] }}</td>
+                            <td>{{ $animal['name']}}</td>
+                            <td>{{ $animal['date_of_birth']}}</td>
+                            @if($animal['available'])
+                                <td>Available</td>
+                            @else
+                                <td>Unavailable</td>
+                            @endif
+                            @if($animal['user_name'])
+                                <td>{{ $animal['user_name']}}</td>
+                            @else
+                                <td>Not Adopted</td>
+                            @endif
+                            <td>
+                                <a href="{{ route('animals.show', ['animal' => $animal['id']]) }}" class="btn btn-blue">Details</a>
+                            </td>
+                            <td>
+                                <a href="{{ route('animals.edit', ['animal' => $animal['id']]) }}" class="btn btn-yellow">Edit</a>
+                            </td>
+                            <td>
+                                <form action="{{ action([App\Http\Controllers\AnimalController::class, 'destroy'], ['animal' => $animal['id']]) }}" method="post">
+                                    @csrf
+                                    <input name="_method" type="hidden" value="DELETE"> <button class="btn btn-red" type="submit">Delete</button>
+                                </form> 
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
