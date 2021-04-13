b652cdedf4247b06f8abfcb365a3149cb61e679a
kelvin589 <190081735@aston.ac.uk>
2021-04-13 22:07:32 +0100

    Update pending requests view
    Add more button styles.
    Add date and time to pending request column.

--- resources/views/requests/pending.blade.php
+++ resources/views/requests/pending.blade.php
@@ -1,67 +1,62 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">Pending Adoption Requests</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if (\Session::has('success'))
-                        <div class="alert alert-success">
-                            <p>{{ \Session::get('success') }}</p>
-                        </div><br />
-                    @elseif (session('failure'))
-                        <div class="alert alert-danger">
-                            <p>{{ \Session::get('failure') }}</p>
-                        </div><br />
-                    @endif
-                    
-                    <table class="table table-striped table-bordered table-hover">
-                        <thead> 
-                            <tr>
-                                <th>Request ID</th>
-                                <th>Username</th>
-                                <th>Animal Name</th>
-                                <th>Adoption Status</th>
-                                <th colspan="3">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($requests as $request)
-                            <tr>
-                                <td>{{ $request->id }}</td>
-                                <td>{{ $request->user_name }}</td>
-                                <td>{{ $request->animal_name }}</td>
-                                <td>{{ $request->adoption_status }}</td>
-                                <td>
-                                    <form method="POST" action="{{ route('update_request_status', ['id' => $request['id']]) }}" enctype="multipart/form-data" >
-                                        @method('PATCH')
-                                        @csrf
-                                        <a href="{{ route('requests.show', ['request' => $request['id']]) }}" class="btn btn-primary">Details</a>
-                                        
-                                        @if(Gate::allows('admin-functionality'))
-                                        <input type="submit" class="btn btn-success" onclick="approve()" name="submitButton" value="Approve" />
-                                        <input type="submit" class="btn btn-danger" onclick="deny()" name="submitButton" value="Deny"/>
-                                        @endif
-                                    </form>
-                                    <form action="{{ action([App\Http\Controllers\RequestController::class, 'destroy'], ['request' => $request['id']]) }}" method="post">
-                                        @csrf
-                                        <input name="_method" type="hidden" value="DELETE"> 
-                                        <button class="btn btn-danger" type="submit">Cancel</button>
-                                    </form> 
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+            <h1 class="centre">Pending Adoption Requests</h1>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            @if (\Session::has('success'))
+                <div class="alert alert-success">
+                    <p>{{ \Session::get('success') }}</p>
+                </div><br />
+            @elseif (session('failure'))
+                <div class="alert alert-danger">
+                    <p>{{ \Session::get('failure') }}</p>
+                </div><br />
+            @endif
+            
+            <table class="table table-striped table-bordered table-hover table-pink">
+                <thead> 
+                    <tr>
+                        <th>Request ID</th>
+                        <th>Animal Name</th>
+                        <th>Request Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($requests as $request)
+                    <tr>
+                        <td>{{ $request->id }}</td>
+                        <td>{{ $request->animal_name }}</td>
+                        <td>{{ date("l jS \of F Y h:i A", strtotime($request->created_at)) }}</td>
+                        <td style="text-align: center;">
+                            <form style="float:left; padding-left:10px; padding-right:10px;" method="POST" action="{{ route('update_request_status', ['id' => $request['id']]) }}" enctype="multipart/form-data" >
+                                @method('PATCH')
+                                @csrf
+                                <a href="{{ route('requests.show', ['request' => $request['id']]) }}" class="btn btn-blue">Details</a>
+                                
+                                @if(Gate::allows('admin-functionality'))
+                                    <input type="submit" class="btn btn-success" name="submitButton" value="Approve"/>
+                                    <input type="submit" class="btn btn-danger" name="submitButton" value="Deny"/>
+                                @endif
+                            </form>
+                            <form style="float:left; padding-left:10px; padding-right:10px; " action="{{ action([App\Http\Controllers\RequestController::class, 'destroy'], ['request' => $request['id']]) }}" method="post">
+                                @csrf
+                                <input name="_method" type="hidden" value="DELETE"> 
+                                <button class="btn btn-red" type="submit">Cancel</button>
+                            </form> 
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
