4766a16a07f0153efa5d0c4d460a9c774e8ba6e6
kelvin589 <190081735@aston.ac.uk>
2021-04-13 22:33:20 +0100

    Update request details show view

--- resources/views/requests/show.blade.php
+++ resources/views/requests/show.blade.php
@@ -2,43 +2,51 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8 ">
-            <div class="card">
-                <div class="card-header">Request Details</div>
-                <div class="card-body">
-                    <table class="table table-striped" border="1" >
-                        <tr>
-                            <th>Request ID</th>
-                            <td>{{ $request['id']}} </td>
-                        </tr>
-                        <tr>
-                            <th><b>Username</th>
-                            <td>{{$request['user_name']}}</td>
-                        </tr>
-                        <tr>
-                            <th>Animal Name</th>
-                            <td>{{$request['animal_name']}}</td>
-                        </tr>
-                        <tr>
-                            <th>Adoption Status</th>
-                            <td>{{ $request['adoption_status'] }}</td>
-                        </tr>
-                        <tr> 
-                            <td colspan='2'>
-                                <img style="width:100%;height:100%" src="{{ asset('storage/images/'.$request->image)}}">
-                            </td>
-                        </tr>
-                    </table>
-                    
-                    <table>
-                        <tr>
-                            <td>
-                                <a href="{{ url()->previous() }}" class="btn btn-primary" role="button">Back to the list</a>
-                            </td>
-                        </tr>
-                    </table> 
-                </div>
-            </div> 
+        <div class="col-md-8">
+            <h1 class="centre">Request Details</h1>
+            <table class="table table-striped table-pink">
+                <tr>
+                    <th>Request ID</th>
+                    <td>{{ $request['id'] }} </td>
+                </tr>
+                <tr>
+                    <th><b>Username</th>
+                    <td>{{ $request['user_name'] }}</td>
+                </tr>
+                <tr>
+                    <th>Request Date</th>
+                    <td>{{ date("l jS \of F Y h:i A", strtotime($request->created_at)) }}</td>
+                </tr>
+                <tr>
+                    <th>Adoption Status</th>
+                    <td>{{ $request['adoption_status'] }}</td>
+                </tr>
+                <tr>
+                    <th>Animal Name</th>
+                    <td>{{ $request['animal_name'] }}</td>
+                </tr>
+                <tr>
+                    <th>Description</th>
+                    <td width="70%">{{ $request['description'] }}</td>
+                </tr>
+                <tr>
+                    <th>Date of Birth</th>
+                    <td>{{ date("jS F Y", strtotime($request->date_of_birth)) }}</td>
+                </tr>
+                <tr> 
+                    <td colspan='2'>
+                        <img style="width:100%;height:100%" src="{{ asset('storage/images/'.$request->image) }}">
+                    </td>
+                </tr>
+            </table>
+            
+            <table>
+                <tr>
+                    <td>
+                        <a href="{{ url()->previous() }}" class="btn btn-pink" role="button">Back to the list</a>
+                    </td>
+                </tr>
+            </table> 
         </div>
     </div>
 </div>
