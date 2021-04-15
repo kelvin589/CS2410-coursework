d14ed3c55311baae14c2d70c53abf8ff1638c24a
kelvin589 <190081735@aston.ac.uk>
2021-04-15 21:48:49 +0100

    Separate animal type selector

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -4,27 +4,7 @@
 @section('title', 'All Animals')
 
 @section('content')
-    <div>
-        <form>
-            <!-- Select does not have a value attribute. Must programatically change the value show. -->
-            @php
-                function checkType($selected_type, $old_type) {
-                    return $selected_type==$old_type ? 'selected' : '';
-                }
-            @endphp
-            <select name="type" id="type" onchange='this.form.submit()'>
-                <option value="" selected disabled hidden>Select the animal type</option>
-                <option {{ checkType('all', old('type')) }} value="all">All</option>
-                <option {{ checkType('mammal', old('type')) }} value="mammal">Mammal</option>
-                <option {{ checkType('bird', old('type')) }} value="bird">Bird</option>
-                <option {{ checkType('reptile', old('type')) }} value="reptile">Reptile</option>
-                <option {{ checkType('amphibian', old('type')) }} value="amphibian">Amphibian</option>
-                <option {{ checkType('fish', old('type')) }} value="fish">Fish</option>
-                <option {{ checkType('invertebrate', old('type')) }} value="invertebrate">Invertebrate</option>
-            </select>
-        </form>
-    </div>
-    
+    @include('elements.select_animal_type')    
     <table class="table table-striped table-bordered table-hover table-pink">
         <thead>
             <tr>
