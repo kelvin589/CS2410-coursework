e51bfca59fc09d405f7d5cdaa3ac615262e8f9c1
kelvin589 <190081735@aston.ac.uk>
2021-04-15 18:44:45 +0100

    Add carousel to requests.show

--- resources/views/requests/show.blade.php
+++ resources/views/requests/show.blade.php
@@ -55,8 +55,28 @@
                     <td width="70%">{{ $request['description'] }}</td>
                 </tr>
                 <tr> 
-                    <td colspan='2'>
-                        <img style="width:100%;height:100%" src="{{ asset('storage/images/'.$request->image) }}">
+                <td colspan="2">
+                        <div id="imageCarousel" class="carousel slide" data-interval="false">
+                            <div class="carousel-inner">
+                                <!-- There must be one active carousel item -->
+                                <div class="carousel-item active" style="height:360px;">
+                                    <img class="d-block w-100" src="{{ asset('storage/images/noimage.jpg') }}" alt="noimage.jpg">
+                                </div>
+                                @foreach(explode("|", $request->image) as $image)
+                                <div class="carousel-item " style="height:360px;">
+                                    <img class="d-block w-100" src="{{ asset('storage/images/'.$image) }}" alt="{{ $image }}">
+                                </div>
+                                @endforeach
+                            </div>
+                            <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
                     </td>
                 </tr>
             </table>
