df0e12a9e8f6578cf47144d16627eab658d272c1
kelvin589 <190081735@aston.ac.uk>
2021-04-15 21:24:15 +0100

    Move layout divs to master app view
    Similar layout for every view. Yield on number of columns and title.

--- resources/views/animals/available.blade.php
+++ resources/views/animals/available.blade.php
@@ -1,81 +1,77 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <h1 class="centre">Available Animals</h1>
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
+@section('columns', '12')
 
-            @if (\Session::has('success'))
-                <div class="alert alert-success">
-                    <p>{{ \Session::get('success') }}</p>
-                </div><br />
-            @endif
-
-            <div>
-                <form>
-                    <!-- Select does not have a value attribute. Must programatically change the value show. -->
-                    @php
-                        function checkType($selected_type, $old_type) {
-                            return $selected_type==$old_type ? 'selected' : '';
-                        }
-                    @endphp
-                    <select name="type" id="type" onchange='this.form.submit()'>
-                        <option value="" selected disabled hidden>Select the animal type</option>
-                        <option {{ checkType('all', old('type')) }} value="all">All</option>
-                        <option {{ checkType('mammal', old('type')) }} value="mammal">Mammal</option>
-                        <option {{ checkType('bird', old('type')) }} value="bird">Bird</option>
-                        <option {{ checkType('reptile', old('type')) }} value="reptile">Reptile</option>
-                        <option {{ checkType('amphibian', old('type')) }} value="amphibian">Amphibian</option>
-                        <option {{ checkType('fish', old('type')) }} value="fish">Fish</option>
-                        <option {{ checkType('invertebrate', old('type')) }} value="invertebrate">Invertebrate</option>
-                    </select>
-                </form>
-            </div>
-
-            <table class="table table-striped table-bordered table-hover table-pink">
-                <thead> 
-                    <tr>
-                        <th>@sortablelink('name', 'Name')</th>
-                        <th>@sortablelink('date_of_birth', 'Date of Birth')</th>
-                        <th>Description</th>
-                        <th>Type</th>
-                        <th colspan="2">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($animals as $animal)
-                    <tr>
-                        <td>{{ $animal->name }}</td>
-                        <td>{{ date("jS F Y", strtotime($animal->date_of_birth)) }}</td>
-                        <td width="42%">{{ $animal->description }}</td>
-                        <td>{{ $animal->type }}</td>
-                        <td>
-                            <a href="{{ route('animals.show', ['animal' => $animal['id']]) }}" class="btn btn-blue">Details</a>
-                        </td>
-                        <!-- Only show button for animals user has not requested -->
-                        @if(App\Models\Request::animalIDUserID($animal->id, Auth::id())->exists())
-                            <td>Requested</td>
-                        @else
-                            <td>
-                                <form method="POST" action="{{ route('request_adoption', ['id' => $animal['id']]) }}" enctype="multipart/form-data" >
-                                    @method('PATCH')
-                                    @csrf
-                                    <input type="submit" class="btn btn-green" name="submitButton" value="Request Adoption" />
-                                </form>
-                            </td>
-                        @endif
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-            {{ $animals->appends(\Request::except('page'))->render() }}
+@section('title', 'Available Animals')
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
         </div>
+    @endif
+
+    @if (\Session::has('success'))
+        <div class="alert alert-success">
+            <p>{{ \Session::get('success') }}</p>
+        </div><br />
+    @endif
+
+    <div>
+        <form>
+            <!-- Select does not have a value attribute. Must programatically change the value show. -->
+            @php
+                function checkType($selected_type, $old_type) {
+                    return $selected_type==$old_type ? 'selected' : '';
+                }
+            @endphp
+            <select name="type" id="type" onchange='this.form.submit()'>
+                <option value="" selected disabled hidden>Select the animal type</option>
+                <option {{ checkType('all', old('type')) }} value="all">All</option>
+                <option {{ checkType('mammal', old('type')) }} value="mammal">Mammal</option>
+                <option {{ checkType('bird', old('type')) }} value="bird">Bird</option>
+                <option {{ checkType('reptile', old('type')) }} value="reptile">Reptile</option>
+                <option {{ checkType('amphibian', old('type')) }} value="amphibian">Amphibian</option>
+                <option {{ checkType('fish', old('type')) }} value="fish">Fish</option>
+                <option {{ checkType('invertebrate', old('type')) }} value="invertebrate">Invertebrate</option>
+            </select>
+        </form>
     </div>
-</div>
+
+    <table class="table table-striped table-bordered table-hover table-pink">
+        <thead> 
+            <tr>
+                <th>@sortablelink('name', 'Name')</th>
+                <th>@sortablelink('date_of_birth', 'Date of Birth')</th>
+                <th>Description</th>
+                <th>Type</th>
+                <th colspan="2">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($animals as $animal)
+            <tr>
+                <td>{{ $animal->name }}</td>
+                <td>{{ date("jS F Y", strtotime($animal->date_of_birth)) }}</td>
+                <td width="42%">{{ $animal->description }}</td>
+                <td>{{ $animal->type }}</td>
+                <td>
+                    <a href="{{ route('animals.show', ['animal' => $animal['id']]) }}" class="btn btn-blue">Details</a>
+                </td>
+                <!-- Only show button for animals user has not requested -->
+                @if(App\Models\Request::animalIDUserID($animal->id, Auth::id())->exists())
+                    <td>Requested</td>
+                @else
+                    <td>
+                        <form method="POST" action="{{ route('request_adoption', ['id' => $animal['id']]) }}" enctype="multipart/form-data" >
+                            @method('PATCH')
+                            @csrf
+                            <input type="submit" class="btn btn-green" name="submitButton" value="Request Adoption" />
+                        </form>
+                    </td>
+                @endif
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $animals->appends(\Request::except('page'))->render() }}
 @endsection
--- resources/views/requests/show.blade.php
+++ resources/views/requests/show.blade.php
@@ -1,94 +1,91 @@
 @extends('layouts.app')
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h1 class="centre">Request Details</h1>
-            <h3>User Request Details</h3>
-            <table class="table table-striped table-bordered table-hover table-pink">
-                <tr>
-                    <th>Request ID</th>
-                    <td>{{ $request['id'] }} </td>
-                </tr>
-                <tr>
-                    <th><b>User ID</th>
-                    <td>{{ $request['user_id'] }}</td>
-                </tr>
-                <tr>
-                    <th><b>Username</th>
-                    <td>{{ $request['user_name'] }}</td>
-                </tr>
-                <tr>
-                    <th><b>Name</th>
-                    <td>{{ $request['forename'] . ' ' . $request['surname'] }}</td>
-                </tr>
-                <tr>
-                    <th>Request Date</th>
-                    <td>{{ date("l jS \of F Y h:i A", strtotime($request->created_at)) }}</td>
-                </tr>
-                <tr>
-                    <th>Adoption Status</th>
-                    <td>{{ $request['adoption_status'] }}</td>
-                </tr>
-            </table>
 
-            <h3>Animal Details</h3>
-            <table class="table table-striped table-bordered table-hover table-pink">
-                <tr>
-                    <th>Animal ID</th>
-                    <td>{{ $request['animal_id'] }} </td>
-                </tr>
-                <tr>
-                    <th>Animal Name</th>
-                    <td>{{ $request['animal_name'] }}</td>
-                </tr>
-                <tr>
-                    <th>Animal Type</th>
-                    <td>{{ $request['type'] }}</td>
-                </tr>
-                <tr>
-                    <th>Date of Birth</th>
-                    <td>{{ date("jS F Y", strtotime($request->date_of_birth)) }}</td>
-                </tr>
-                <tr>
-                    <th>Description</th>
-                    <td width="70%">{{ $request['description'] }}</td>
-                </tr>
-                <tr> 
-                <td colspan="2">
-                        <div id="imageCarousel" class="carousel slide" data-interval="false">
-                            <div class="carousel-inner">
-                                <!-- There must be one active carousel item -->
-                                <div class="carousel-item active" style="height:360px;">
-                                    <img class="d-block w-100" src="{{ asset('storage/images/noimage.jpg') }}" alt="noimage.jpg">
-                                </div>
-                                @foreach(explode("|", $request->image) as $image)
-                                <div class="carousel-item " style="height:360px;">
-                                    <img class="d-block w-100" src="{{ asset('storage/images/'.$image) }}" alt="{{ $image }}">
-                                </div>
-                                @endforeach
-                            </div>
-                            <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
+@section('columns', '8')
+@section('title', 'Request Details')
+
+@section('content')
+    <h3>User Request Details</h3>
+    <table class="table table-striped table-bordered table-hover table-pink">
+        <tr>
+            <th>Request ID</th>
+            <td>{{ $request['id'] }} </td>
+        </tr>
+        <tr>
+            <th><b>User ID</th>
+            <td>{{ $request['user_id'] }}</td>
+        </tr>
+        <tr>
+            <th><b>Username</th>
+            <td>{{ $request['user_name'] }}</td>
+        </tr>
+        <tr>
+            <th><b>Name</th>
+            <td>{{ $request['forename'] . ' ' . $request['surname'] }}</td>
+        </tr>
+        <tr>
+            <th>Request Date</th>
+            <td>{{ date("l jS \of F Y h:i A", strtotime($request->created_at)) }}</td>
+        </tr>
+        <tr>
+            <th>Adoption Status</th>
+            <td>{{ $request['adoption_status'] }}</td>
+        </tr>
+    </table>
+
+    <h3>Animal Details</h3>
+    <table class="table table-striped table-bordered table-hover table-pink">
+        <tr>
+            <th>Animal ID</th>
+            <td>{{ $request['animal_id'] }} </td>
+        </tr>
+        <tr>
+            <th>Animal Name</th>
+            <td>{{ $request['animal_name'] }}</td>
+        </tr>
+        <tr>
+            <th>Animal Type</th>
+            <td>{{ $request['type'] }}</td>
+        </tr>
+        <tr>
+            <th>Date of Birth</th>
+            <td>{{ date("jS F Y", strtotime($request->date_of_birth)) }}</td>
+        </tr>
+        <tr>
+            <th>Description</th>
+            <td width="70%">{{ $request['description'] }}</td>
+        </tr>
+        <tr> 
+        <td colspan="2">
+                <div id="imageCarousel" class="carousel slide" data-interval="false">
+                    <div class="carousel-inner">
+                        <!-- There must be one active carousel item -->
+                        <div class="carousel-item active" style="height:360px;">
+                            <img class="d-block w-100" src="{{ asset('storage/images/noimage.jpg') }}" alt="noimage.jpg">
                         </div>
-                    </td>
-                </tr>
-            </table>
-            
-            <table>
-                <tr>
-                    <td>
-                        <a href="{{ url()->previous() }}" class="btn btn-pink" role="button">Back to the list</a>
-                    </td>
-                </tr>
-            </table> 
-        </div>
-    </div>
-</div>
+                        @foreach(explode("|", $request->image) as $image)
+                        <div class="carousel-item " style="height:360px;">
+                            <img class="d-block w-100" src="{{ asset('storage/images/'.$image) }}" alt="{{ $image }}">
+                        </div>
+                        @endforeach
+                    </div>
+                    <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </td>
+        </tr>
+    </table>
+    
+    <table>
+        <tr>
+            <td>
+                <a href="{{ url()->previous() }}" class="btn btn-pink" role="button">Back to the list</a>
+            </td>
+        </tr>
+    </table> 
 @endsection
