9e4cae07104dbc9376d98eeb0f6936790ac44cbb
kelvin589 <190081735@aston.ac.uk>
2021-04-16 17:41:41 +0100

    Update titles based on page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,10 +25,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-blue shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <!-- {{ config('app.name', 'Laravel') }} -->
-                    Animal Sanctuary
-                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">Animal Sanctuary</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
